2037d2c3b77ea35eb3a1309454c6143396bc4767
Dinesh Tailor <DineshKumar.Tailor@alepo.com>
2019-03-26 18:41:42 +0530

    first commit

---
+++ build/precache-manifest.115aa242b90ea42f6ab5dc6668687ded.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e181b1eb55f60f830e2a",
+    "url": "/static/js/main.b32f8aa0.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "e181b1eb55f60f830e2a",
+    "url": "/static/css/main.0107147e.chunk.css"
+  },
+  {
+    "revision": "ffd5ee68d96758576ec56f3014d4083d",
+    "url": "/index.html"
+  }
+];
